58142ca112045f85d7f1ee50f1fa7afc4cad9963
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-04-18 16:58:11 +0200

    Update supported platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "Toolbox",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14)
+    ],
     products: [
         .library(
             name: "Toolbox",
